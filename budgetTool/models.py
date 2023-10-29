3d7693b273eeea8da461698dee839ca3ef4cae61
Steven <panzerchong@gmail.com>
2023-10-29 01:57:13 -0700

    rateTable

--- budgetTool/models.py
+++ budgetTool/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class RateTable(models.Model):
+    created_at=models.DateTimeField(auto_now_add=True)
+    type=models.CharField(max_length=100)
+    list=models.IntegerField()
+    cost=models.IntegerField()
+
+def __str__(self):
+    return (f"{self.type} {self.list} {self.cost}")
+    
--- budgetTool/views.py
+++ budgetTool/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .models import RateTable
+
 
 def home(request):
     if request.method == 'POST':
@@ -25,3 +27,8 @@ def logout_user(request):
     logout(request)
     messages.success(request,"You have been logged out...")
     return redirect('home')
+
+def rate_table(request):
+    table=RateTable.objects.all()
+
+    return messages.success(request,"it's ratetable\n")
